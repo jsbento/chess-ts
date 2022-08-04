875044e2f2f7554fa23ab781affe6cc1843e265b
jsbento <benton_jacob@yahoo.com>
2022-08-03 22:18:28 -0500

    Cleanup

--- components/board/Piece.tsx
+++ components/board/Piece.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Piece } from "../../types/chess/Piece";
 import { getPieceImg } from "../../utils/pieces/PieceUtils";
 import { useDrag, DragSourceMonitor } from "react-dnd";
-// https://codesandbox.io/s/github/react-dnd/react-dnd/tree/gh-pages/examples_ts/02-drag-around/custom-drag-layer?from-embed=&file=/src/CustomDragLayer.tsx
+
 const Piece: React.FC<Piece> = ({ type, position }) => {
     const [{ isDragging }, drag] = useDrag({
         type: "piece",
