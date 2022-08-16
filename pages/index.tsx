49102226ee0b98d8d59dd58d68b3807b21aca480
jsbento <benton_jacob@yahoo.com>
2022-08-15 19:30:08 -0500

    Prep index for rework

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,36 +16,6 @@ const Home: NextPage = () => {
         <h1 className="text-6xl">
           Welcome to Chess-TS!
         </h1>
-
-        <div className={styles.grid}>
-          <Link href="/chess" className={styles.card}>
-            <h2>Play &rarr;</h2>
-            <p>Play a game of chess!</p>
-          </Link>
-
-          <Link href="/about" className={styles.card}>
-            <h2>About &rarr;</h2>
-            <p>Read a quick overview of how the project was built.</p>
-          </Link>
-
-          <Link
-            href="https://github.com/jsbento/chess-ts"
-            className={styles.card}
-          >
-            <h2>The Code &rarr;</h2>
-            <p>Browse through the source code.</p>
-          </Link>
-
-          {/* <Link
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </Link> */}
-        </div>
       </main>
 
       <footer className={styles.footer}>
