3fd3f577a9959f4165ad3c7f2dc3f43219c78b37
Jake Benton <benton_jacob@yahoo.com>
2024-03-17 23:08:56 -0600

    analytics

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Analytics } from '@vercel/analytics/react'
 import Footer from './Footer'
 import NavBar from './NavBar'
 
 const Layout: React.FC<{children: React.ReactNode}> = ({ children }) => {
   return (
     <div className="flex flex-col h-[100vh]">
+      <Analytics />
       <NavBar />
         <div className="flex h-[80vh] items-center">
           {children}
